4840ba98a3f8986c0c74a5694834eb05451f5a9b
Jeferson Slywitch <jeferson@totalhealth.com.br>
2020-02-24 07:20:56 -0300

    merge code 2

--- App.js
+++ App.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
-
-export default class App extends Component {
-  render(){
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Projeto Calculadora</Text>
-      <Text style={styles.sectionDescription}>
-          Vamos calcular!!
-      </Text>
-    </View>  
-  }
-}
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-=======
 import React, { Component } from 'react'
 import { 
     Platform,
@@ -147,4 +88,3 @@ const styles = StyleSheet.create ({
     flexWrap: 'wrap',
   }
 })
->>>>>>> 326c457784c67841d6d03a865abc11245f729ab4
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native'
-
-const style = StyleSheet.create ({
-=======
 import { 
     StyleSheet,
     Text,
@@ -12,7 +7,6 @@ import {
 } from 'react-native'
 
 const styles = StyleSheet.create({
->>>>>>> 326c457784c67841d6d03a865abc11245f729ab4
     button: {
         fontSize: 40,
         height: Dimensions.get('window').width / 4,
@@ -22,8 +16,6 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888',
-<<<<<<< HEAD
-=======
     },
     operationButton: {
         color: '#fff',
@@ -34,15 +26,10 @@ const styles = StyleSheet.create({
     },
     buttonTriple: {
         width: (Dimensions.get('window').width / 4) * 3,
->>>>>>> 326c457784c67841d6d03a865abc11245f729ab4
     }
 })
 
 export default props => {
-<<<<<<< HEAD
-    return(
-        <TouchableHighlight onPress={props.onClick}></TouchableHighlight>
-=======
     const stylesButton = [styles.button]
     if (props.double) stylesButton.push(styles.buttonDouble)
     if (props.triple) stylesButton.push(styles.buttonTriple)
@@ -51,6 +38,5 @@ export default props => {
         <TouchableHighlight onPress={() => props.onClick( props.label )}>
             <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
->>>>>>> 326c457784c67841d6d03a865abc11245f729ab4
     )
 }
